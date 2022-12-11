df85e0ffe3b75129f2f7c4b9e2b95f6ce4a16b61
Anudiss <liobara31@gmail.com>
2022-12-11 22:21:38 +0300

    Добавьте файлы проекта.

---
+++ BuildingCompany/Components/TextInput.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace BuildingCompany.Components
+{
+    public class TextInput : TextBox
+    {
+        #region Placeholder
+        public string Placeholder
+        {
+            get { return (string)GetValue(PlaceholderProperty); }
+            set { SetValue(PlaceholderProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Placeholder.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PlaceholderProperty =
+            DependencyProperty.Register("Placeholder", typeof(string), typeof(TextInput));
+        #endregion
+
+        public TextInput() =>
+            OverridesDefaultStyleProperty.OverrideMetadata(typeof(TextInput), new PropertyMetadata(typeof(TextInput)));
+    }
+}
